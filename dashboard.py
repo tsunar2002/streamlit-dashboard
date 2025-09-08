d03534b65f71a16c8136a9244bc0d8a049a6a712
tzunar <118147365+tsunar2002@users.noreply.github.com>
2025-09-07 23:34:16 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-import plotly.express as px
+# import plotly.express as px
 
 st.set_page_config(
     page_title="US Population Dashboard",
